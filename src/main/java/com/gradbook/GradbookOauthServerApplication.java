f0674fd40286c493216f1e21e7c5eb3e2586d978
munna <monjurmorshed794@outlook.com>
2017-06-18 23:12:50 +0600

    first commit

---
+++ src/main/java/com/gradbook/GradbookOauthServerApplication.java
@@ -0,0 +1,12 @@
+package com.gradbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradbookOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradbookOauthServerApplication.class, args);
+	}
+}
