3b5d1609b77effb0d8dbd22b11acbb3ccfdb51ef
munna <monjurmorshed793@gmail.com>
2017-08-09 00:50:43 +0600

    Logger added.

--- src/main/java/com/gradbook/GradbookOauthServerApplication.java
+++ src/main/java/com/gradbook/GradbookOauthServerApplication.java
@@ -2,12 +2,16 @@ package com.gradbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
 @EnableResourceServer
+@EnableAspectJAutoProxy
+@EnableKafka
 public class GradbookOauthServerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/gradbook/config/OauthAspect.java
@@ -0,0 +1,27 @@
+package com.gradbook.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class OauthAspect {
+
+    /*@Autowired
+    private KafkaTemplate<String, String> template;*/
+
+
+
+    @After("execution( *  com.gradbook..*.*(..))")
+    public void tracing(JoinPoint joinPoint){
+//        this.template.send("ums_logger","The method executed--->"+joinPoint.getSignature().toString());
+        System.out.println("the logger--->"+joinPoint.getSignature().toString());
+    }
+
+}
